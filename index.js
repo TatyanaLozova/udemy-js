1c4ae5e35234aee80f2b6c7111b4dd63b82f9b2d
TatyanaLozova <68035704+TatyanaLozova@users.noreply.github.com>
2022-01-18 00:32:31 +0200

    +

--- index.js
+++ index.js
@@ -34,4 +34,14 @@ const personalMovieDB = {
 
 console.log(personalMovieDB);
 
+const a = prompt('Один из последних просмотренных фильмов?'),
+b = prompt('На сколько оцените его?')
+c = prompt('Один из последних просмотренных фильмов?'),
+d = prompt('На сколько оцените его?')
 
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
